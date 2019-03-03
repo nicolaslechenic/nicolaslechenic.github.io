6b4212d98ba9c4cc90f397f2680f9a81b4a8913c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-03 14:04:22 +0000

    Deploy nicolaslechenic/nicolaslechenic.github.io to github.com/nicolaslechenic/nicolaslechenic.github.io.git:master

--- README.md
+++
@@ -1,2 +0,0 @@
-# nicolaslechenic
-Site web
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-const yieldNode = document.querySelector('#yield')
-
-render(
-  <h1>Hello, world!</h1>,
-  yieldNode
-)
